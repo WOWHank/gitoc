3d807b6b5d34ff5a2ca9e9dbcc1cadebdef1f1ee
WOWHank <33889011+WOWHank@users.noreply.github.com>
2017-11-27 09:22:25 +0800

    Initial Commit

---
+++ gitoc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitoc
+//
+//  Created by Hank_Kuo on 2017/11/27.
+//  Copyright © 2017年 Hank_Kuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitoc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitoc
+//
+//  Created by Hank_Kuo on 2017/11/27.
+//  Copyright © 2017年 Hank_Kuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
